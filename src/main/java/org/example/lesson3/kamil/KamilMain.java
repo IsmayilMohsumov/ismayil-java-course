53bf18460038560fd057e78f3f625a5d7919516d
jumajumayev <jumayev0407@gmail.com>
2024-02-11 17:47:57 +0100

    Yeni

--- src/main/java/org/example/lesson3/kamil/KamilMain.java
+++ src/main/java/org/example/lesson3/kamil/KamilMain.java
@@ -1,7 +1,35 @@
 package org.example.lesson3.kamil;
 
 public class KamilMain {
+
     public static void main(String[] args) {
-        
+     KamilMain kamilMain = new KamilMain();
+     kamilMain.Kamil("Kamil");
     }
-}
+
+    public String Kamil(String a){
+        return a;
+
+
+
+
+
+    }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/org/example/lesson4/juma/homework/Calculator.java
+++ src/main/java/org/example/lesson4/juma/homework/Calculator.java
@@ -1,13 +1,44 @@
 package org.example.lesson4.juma.homework;
 
 import java.util.Scanner;
+class CalculatorDemo{
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+
+        double three = calculator.add(3.2, 3.5, 4.2);
+        System.out.println(three);
+
+        double two = calculator.add(3.0, 5.3);
+        System.out.println(two);
+
+        calculator.compare();
+
+        //calculator.calculatorName = "String";
+        //We need to change the calculatorName's access modifier to the other ones to be able to use it in the different classes.
+        // These access modifiers can be: Public, Protected or Default.
+
+
+    }
+
+
+}
 
 public class Calculator {
 
 
+    public Calculator{
+     double num1 = 5;
+     double num2 = 6;
+    }
+    private Calculator{
+        String calculatorName = "Hello";
+    }
+    public Calculator(int num2){
+     num2 = 6;
 
-    public double num1;
-    public double num2;
+
+    }
+
     private String calculatorName;
 
     public double add(double num1, double num2){
@@ -21,34 +52,22 @@ public class Calculator {
     }
 
     public void compare(){
-        System.out.println("Enter number 1: ");
-        Scanner sc = new Scanner(System.in);
-        num1 = sc.nextDouble();
-        System.out.println("Enter number 2: ");
-        num2 = sc.nextDouble();
-        if(num1 > num2){
-            System.out.println("Bigger number is "+num1);
-        }else if(num2 > num1){
-            System.out.println("Bigger number is "+num2);
+        System.out.println("Number 1 is:  ");
+
+
+
+
+
+        System.out.println("Number 2 is : ");
+
+        if(true){
+            System.out.println("Bigger number is ");
+        }else if(false){
+            System.out.println("Bigger number is ");
         }else {
             System.out.println("They are the same");
         }
 
     }
 }
-    class CalculatorDemo{
-    public static void main(String[] args) {
-     Calculator Calculator = new Calculator();
-     double three = Calculator.add(3.2, 3.5, 4.2);
-        System.out.println(three);
 
-     double two = Calculator.add(3.0, 5.3);
-        System.out.println(two);
-
-        Calculator.compare();
-        Calculator.calculatorName = "String";
-        //We need to change the calculatorName's access modifier to the other ones to be able to use it in the different classes.
-        // These access modifiers can be: Public, Protected or Default.
-
-    }
-}
